d3d72e6d1d7d702b3197b838f8e3a9d711cc3192
ravirawat0 <ravirawat0@outlook.com>
2024-07-03 20:07:59 +0530

    f:completed structure of main componenet.

--- Blog/src/app/app.component.ts
+++ Blog/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SidebarComponent} from "./Components/sidebar/sidebar.component";
+import {MainContainerComponent} from "./Components/main-container/main-container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, MainContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
